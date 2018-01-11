3d423060f4f41fb39a2102bdedb3fc8381d61041
RachelMustakis <Rachush2@walla.com>
2018-01-11 22:05:16 +0200

    main update to start all files

--- src/Matala.java
+++ src/Matala.java
@@ -4,13 +4,17 @@
  * @author Rachel
  */
 public class Matala { 
-	private static final String FILENAME = "C:\\Users\\Rachel\\Downloads\\study\\OR\\StrongestWifi.csv";
+	private static final String FILENAME1 = "C:\\Users\\Rachel\\Downloads\\study\\OR\\Ex2\\_BM2_comb_all_.csv";
+	private static final String FILENAME2 = "C:\\Users\\Rachel\\Downloads\\study\\OR\\Ex2\\_comb_no_gps_ts1.csv";
 
 	public static void main(String[] args) {
 
 		ReadFolderWriteCsv.csv();
-	 	KmlReadWrite.readWrite();
-		System.out.println(routerLocation.location(FILENAME, "2135as"));
+		KmlReadWrite.readWrite();
+		MyLocaion.location(FILENAME1,FILENAME2,4);
+		routerLocation.location(FILENAME1,3);
+
+
 	}
 
 }
